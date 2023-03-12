c4b7995779aecae09b085897314b4002276f40e8
eliadelmoral <elia.m.zamorano@gmail.com>
2023-03-12 20:00:49 +0100

    holi

---
+++ prueba.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int num, i;
+
+    cout << "Inserte un numero: ";
+
+    cin >> num;
+
+    for (i=1; i<=num; i++)
+    {
+        cout << i << "\n";
+
+    }
+
+    return 0;
+}
